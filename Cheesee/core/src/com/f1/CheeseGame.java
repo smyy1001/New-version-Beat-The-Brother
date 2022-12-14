7743c015d8bade4089d2db8f3621b577d70d411c
YusufToraman <91420577+YusufToraman@users.noreply.github.com>
2022-12-14 14:49:57 +0300

    Frame Size is changed

--- Cheesee/core/src/com/f1/CheeseGame.java
+++ Cheesee/core/src/com/f1/CheeseGame.java
@@ -1,9 +1,10 @@
 package com.f1;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 
 public class CheeseGame extends Game{
-
     public void create(){
+        Gdx.graphics.setWindowedMode(1230, 1230);
         CheeseMenu cm = new CheeseMenu(this);
         setScreen(cm);
     }
